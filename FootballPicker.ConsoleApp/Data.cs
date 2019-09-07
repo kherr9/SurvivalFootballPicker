dc3af1537234ae64a6d7dd58a4bf861854119f0f
Kevin Herr <kherr@mediafly.com>
2019-09-07 09:20:07 -0500

    read data

---
+++ FootballPicker.ConsoleApp/Data.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FootballPicker.ConsoleApp
+{
+    public static class Data
+    {
+        public static Rank[] LoadRankings()
+        {
+            return File.ReadAllLines("ranking.csv")
+                .Skip(1)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Split(','))
+                .Select(row => new Rank
+                {
+                    Team = row[0],
+                    Ranking = int.Parse(row[1])
+                })
+                .ToArray();
+        }
+
+        public static Match[] LoadMatches()
+        {
+            return File.ReadAllLines("schedule.csv")
+                .Skip(1)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Split(','))
+                .Select(row =>
+                    new
+                    {
+                        Team = row[0],
+                        Weeks = row.Skip(1).ToArray()
+                    })
+                .SelectMany(row =>
+                {
+                    var result = new List<Match>();
+                    for (var i = 0; i < row.Weeks.Length; i++)
+                    {
+                        var week = i + 1;
+                        var otherTeam = row.Weeks[i];
+
+                        // skip when other team is home
+                        // because the matches are duplicated.
+                        // this is an easy way to de-dupe
+                        // and keep logic simple
+                        if (otherTeam[0] == '@')
+                            continue;
+
+                        result.Add(new Match
+                        {
+                            Week = week,
+                            HomeTeam = row.Team,
+                            AwayTeam = otherTeam
+                        });
+                    }
+
+                    return result;
+                })
+                .OrderBy(x => x.Week)
+                .ThenBy(x => x.HomeTeam)
+                .ToArray();
+        }
+    }
+}
--- FootballPicker.ConsoleApp/Program.cs
+++ FootballPicker.ConsoleApp/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace FootballPicker.ConsoleApp
@@ -11,14 +9,14 @@ namespace FootballPicker.ConsoleApp
         {
             Console.WriteLine("Hello World!");
 
-            foreach (var rank in ReadRankings().OrderBy(x => x.Ranking))
+            foreach (var rank in Data.LoadRankings().OrderBy(x => x.Ranking))
             {
                 Console.WriteLine(rank.ToString());
             }
 
             Console.WriteLine();
 
-            var matches = ReadSchedule();
+            var matches = Data.LoadMatches();
 
             var arizonaSchedule = matches.Where(m => m.IsPlaying("ARI")).OrderBy(x => x.Week);
 
@@ -33,62 +31,6 @@ namespace FootballPicker.ConsoleApp
             {
                 Console.WriteLine($"Week:{match.Week,-2}, Home:{match.HomeTeam,-3}, Away:{match.AwayTeam,-3}");
             }
-        }
-
-        private static Rank[] ReadRankings()
-        {
-            return File.ReadAllLines("ranking.csv")
-                .Skip(1)
-                .Where(line => !string.IsNullOrWhiteSpace(line))
-                .Select(line => line.Split(','))
-                .Select(row => new Rank
-                {
-                    Team = row[0],
-                    Ranking = int.Parse(row[1])
-                })
-                .ToArray();
-        }
-
-        private static Match[] ReadSchedule()
-        {
-            return File.ReadAllLines("schedule.csv")
-                .Skip(1)
-                .Where(line => !string.IsNullOrWhiteSpace(line))
-                .Select(line => line.Split(','))
-                .Select(row =>
-                    new
-                    {
-                        Team = row[0],
-                        Weeks = row.Skip(1).ToArray()
-                    })
-                .SelectMany(row =>
-                {
-                    var result = new List<Match>();
-                    for (var i = 0; i < row.Weeks.Length; i++)
-                    {
-                        var week = i + 1;
-                        var otherTeam = row.Weeks[i];
-
-                        // skip when other team is home
-                        // because the matches are duplicated.
-                        // this is an easy way to de-dupe
-                        // and keep logic simple
-                        if (otherTeam[0] == '@')
-                            continue;
-
-                        result.Add(new Match
-                        {
-                            Week = week,
-                            HomeTeam = row.Team,
-                            AwayTeam = otherTeam
-                        });
-                    }
-
-                    return result;
-                })
-                .OrderBy(x => x.Week)
-                .ThenBy(x => x.HomeTeam)
-                .ToArray();
         }
     }
 }

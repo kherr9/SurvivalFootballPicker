6e680e0c5b284cba562141da1b66e42406529035
Kevin Herr <kherr@mediafly.com>
2019-09-07 08:43:07 -0500

    add picket

---
+++ FootballPicker.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FootballPicker.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

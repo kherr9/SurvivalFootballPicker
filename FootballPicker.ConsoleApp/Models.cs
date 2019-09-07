9ed070b8a65dbe575f42b04f84d02c99bbff7dfb
Kevin Herr <kherr@mediafly.com>
2019-09-07 08:56:04 -0500

    read ranking csv

---
+++ FootballPicker.ConsoleApp/Models.cs
@@ -0,0 +1,20 @@
+﻿namespace FootballPicker.ConsoleApp
+{
+    public struct Rank
+    {
+        public string Team;
+        public int Ranking;
+
+        public override string ToString()
+        {
+            return $"{Team,-3}:{Ranking}";
+        }
+    }
+
+    public struct Schedule
+    {
+        public int Week;
+        public string HomeTeam;
+        public string AwayTeam;
+    }
+}
--- FootballPicker.ConsoleApp/Program.cs
+++ FootballPicker.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace FootballPicker.ConsoleApp
 {
@@ -7,6 +9,25 @@ namespace FootballPicker.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            foreach (var rank in ReadRankings().OrderBy(x => x.Ranking))
+            {
+                Console.WriteLine(rank.ToString());
+            }
+        }
+
+        private static Rank[] ReadRankings()
+        {
+            return File.ReadAllLines("ranking.csv")
+                .Skip(1)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Split(','))
+                .Select(row => new Rank
+                {
+                    Team = row[0],
+                    Ranking = int.Parse(row[1])
+                })
+                .ToArray();
         }
     }
 }

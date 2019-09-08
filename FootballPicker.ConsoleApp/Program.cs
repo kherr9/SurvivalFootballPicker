4f0f4b6eccca12a3b9321a842c5ce0ef2f0acc94
Kevin Herr <kherr@mediafly.com>
2019-09-08 08:39:13 -0500

    add greedy 2

--- FootballPicker.ConsoleApp/Program.cs
+++ FootballPicker.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FootballPicker.ConsoleApp
+﻿using System;
+
+namespace FootballPicker.ConsoleApp
 {
     class Program
     {
@@ -7,7 +9,11 @@
             var ranks = Data.LoadRankings();
             var matches = Data.LoadMatches();
 
+            Console.WriteLine("\n*** simple greedy ***");
             SimpleGreedyPicker.Choose(matches, ranks);
+
+            Console.WriteLine("\n*** simple greedy 2 ***");
+            SimpleGreedyPicker2.Choose(matches, ranks);
         }
     }
 }
---
+++ FootballPicker.ConsoleApp/SimpleGreedyPicker2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FootballPicker.ConsoleApp
+{
+    /// <summary>
+    /// Chooses the team w/ the largest rank difference.
+    /// Select max team for min week
+    /// </summary>
+    public static class SimpleGreedyPicker2
+    {
+        public static void Choose(Match[] input, Rank[] rankings)
+        {
+            var rankingLookup = rankings.ToDictionary(r => r.Team, r => r.Ranking);
+
+            // calculate selection heuristic
+            var selections = input.SelectMany(match =>
+            {
+                var week = match.Week;
+
+                var homeTeam = match.HomeTeam;
+                var homeTeamRanking = rankingLookup[homeTeam];
+
+                var awayTeam = match.AwayTeam;
+                var awayTeamRanking = rankingLookup[awayTeam];
+
+                // FACT 60% of games are won at home
+                const int homeFieldAdvantageWeight = 4;
+
+                return new List<Selection>
+                {
+                    new Selection
+                    {
+                        Week = week,
+                        Team = homeTeam,
+                        Ranking = homeTeamRanking,
+                        Value = awayTeamRanking - homeTeamRanking + homeFieldAdvantageWeight,
+                        Opponent = awayTeam,
+                        OpponentRanking = awayTeamRanking,
+                        HomeTeam = homeTeam
+                    },
+                    new Selection
+                    {
+                        Week = week,
+                        Team = awayTeam,
+                        Ranking = awayTeamRanking,
+                        Value = homeTeamRanking - awayTeamRanking - homeFieldAdvantageWeight,
+                        Opponent = homeTeam,
+                        OpponentRanking = homeTeamRanking,
+                        HomeTeam = homeTeam
+                    }
+                };
+            }).ToList();
+
+            var picks = new List<Selection>();
+
+            while (selections.Any())
+            {
+                // find the max pick for each week
+                var pick = selections
+                    .GroupBy(s => s.Week)
+                    .Select(grp =>
+                        grp.OrderByDescending(s => s.Value)
+                            .ThenByDescending(s => s.Ranking)
+                            .First())
+                    .OrderBy(s => s.Value)
+                    .ThenBy(s => s.Ranking)
+                    .First();
+
+                picks.Add(pick);
+
+                // remove selected team and week from selections
+                selections = selections.Where(s => picks.All(p => p.Team != s.Team)).ToList();
+                selections = selections.Where(s => picks.All(p => p.Week != s.Week)).ToList();
+            }
+
+            foreach (var pick in picks.OrderBy(p => p.Week))
+            {
+                Console.WriteLine(pick.Print());
+            }
+        }
+
+        private struct Selection
+        {
+            public int Week;
+            public string Team;
+            public int Ranking;
+            public int Value;
+            public string Opponent;
+            public int OpponentRanking;
+            public string HomeTeam { get; set; }
+
+            public string Print()
+            {
+                return $"Week:{Week,-2} Team:{TeamFormatted(),-4} TeamRank:{Ranking,-2} Value:{Value,-2} Opponent:{OpponentFormatted(),-4} OpponentRank:{OpponentRanking,-2}";
+            }
+
+            private string TeamFormatted()
+            {
+                if (Team == HomeTeam)
+                    return $"@{Team}";
+
+                return Team;
+            }
+
+            private string OpponentFormatted()
+            {
+                if (Opponent == HomeTeam)
+                    return $"@{Opponent}";
+
+                return Opponent;
+            }
+        }
+    }
+}
